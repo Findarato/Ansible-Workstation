126ebd1cab0b52b7b3ddc984eeae780235af43b2
Joseph Harry <findarato@gmail.com>
2016-04-30 08:42:26 -0500

    Added gitKracken download and install to developer

---
+++ setupEnvironment.sh
@@ -0,0 +1,19 @@
+
+
+# *************************************************************** #
+# *  We need to do a few things before we start.                * #
+# *  Setup ssh keys for user.                                   * #
+# *  Push the keys to all of the hosts.                         * #
+# *  Install Ansible                                            * #
+# *  Clone the repo                                             * #
+# *************************************************************** #
+
+
+# Automate the generation and distribution of keys
+# https://www.ibm.com/developerworks/community/blogs/kevgrig/entry/automating_server_administration_with_ssh_keys?lang=en
+#
+mkdir ~/.ssh
+mkdir ~/.ssh && chmod 700 ~/.ssh && cd ~/.ssh/
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/remote
+ssh-copy-id -i ~/.ssh/remote user@host
+ssh-add ~/.ssh/remote
