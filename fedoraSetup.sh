56aaa262871b5bb6020cf27930f2d939209cfeb0
Joseph Harry <findarato@gmail.com>
2016-06-23 10:19:06 -0500

    added a gitignore for retry files. Also working on getting my desktop deployed correctly with ansible. This should be a one time thing, hopefully

--- fedoraSetup.sh
+++ fedoraSetup.sh
@@ -6,8 +6,12 @@ echo "Starting Deployment"
 mkdir ~/setup
 cd ~/setup
 git clone https://github.com/Findarato/Ansible-Workstation.git .
+
+echo "Setting up Ansible Roles"
 ansible-galaxy install -r requirements.txt
 
+echo "Running Ansible"
+ansible-playbook -i localHosts ProvisionDesktop.yml
 
 #Clean up the Setup
 echo "Cleaning up Setup directory"
