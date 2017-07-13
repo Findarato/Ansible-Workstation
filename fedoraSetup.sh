f9684cd5516a5554f71d58e5da028211ac3fb0eb
Joseph Harry <findarato@gmail.com>
2017-07-13 14:14:27 -0500

    :bug:fix(basic): Basic role is working mostly - Still need to fix the git cloning of dot files Signed-off-by: Joseph Harry <findarato@gmail.com>

--- fedoraSetup.sh
+++ fedoraSetup.sh
@@ -1,21 +1,47 @@
+#!/usr/bin/env bash
+
+if [ "$EUID" -eq 0 ]
+  then 
+    echo "Please do not run as root"
+    echo "$EUID"
+  exit
+fi
+
 echo "Ensuring no previous Setup"
-rm -rf ~/setup
+rm -rf "$HOME/setup"
 
 echo "Updating System"
-dnf update -y
-dnf install -y ansible git python-pip python-dnf python-pip python ansible ansible-lint
+sudo dnf distro-sync -y
+sudo dnf install -y ansible git python-pip python-dnf python-pip python ansible ansible-lint
+
+echo "Working in the $HOME Directory"
+
+echo "Getting Dotfiles"
+mkdir -p "$HOME/Documents/src"
+cd "$HOME/Documents/src"
+git clone https://github.com/Findarato/dotFiles.git dotFiles
+cd "$HOME/Documents/src/dotFiles"
+
+echo "Linking files"
+. linkup.sh
+
+source "$HOME/.bashrc"
+source "$HOME/.ansible.cfg"
 
 echo "Starting Deployment"
-mkdir ~/setup
-cd ~/setup
-git clone https://github.com/Findarato/Ansible-Workstation.git .
+mkdir -p "$HOME/setup"
+cd "$HOME/setup"
+git clone https://github.com/Findarato/Ansible-Workstation.git Ansible-Workstaton
+
+echo "Chaning to Ansible directory"
+cd "$HOME/setup/Ansible-Workstaton"
 
 echo "Setting up Ansible Roles"
 ansible-galaxy install -r requirements.yml
 
 echo "Running Ansible"
-ansible-playbook -i localHosts ProvisionDesktop.yml
+sudo ansible-playbook -i localHosts ProvisionDesktop.yml
 
 #Clean up the Setup
 echo "Cleaning up Setup directory"
-rm -rf ~/setup
+#rm -rf $HOME/setup
