ef7320c8326058e48e21603e2591530fa4215b10
Joseph Harry <findarato@gmail.com>
2019-02-08 14:30:33 -0600

    :bug: fix(pass): Pulling down password store Signed-off-by: Joseph Harry <findarato@gmail.com>

--- fedoraSetup.sh
+++ fedoraSetup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -eq 0 ]; then
+if [ "${EUID}" -eq 0 ]; then
     echo "Please do not run as root"
-    echo "$EUID"
+    echo "${EUID}"
     exit
 fi
 
-SETUPDIR="$HOME/setup/"
+SETUPDIR="${HOME}/setup/"
 
 echo "Ensuring no previous Setup"
 rm -rf "$SETUPDIR"
@@ -14,33 +14,33 @@ rm -rf "$SETUPDIR"
 echo "Updating System"
 sudo dnf distro-sync -y
 sudo dnf groupinstall "Ansible node" -y
-sudo dnf install -y ansible git python-pip python-dnf python-pip python ansible ansible-lint
+sudo dnf install -y ansible git python-pip python-dnf python-pip python ansible ansible-lint pass
 
-echo "Working in the $SETUPDIR Directory"
+echo "Working in the ${SETUPDIR} Directory"
 
-mkdir -p "$SETUPDIR"
-cd "$SETUPDIR"
+mkdir -p "${SETUPDIR}"
+cd "${SETUPDIR}"
 
 
-wget "https://raw.githubusercontent.com/Findarato/dotFiles/master/.bashrc" -O "$HOME/.bashrc"
-wget "https://raw.githubusercontent.com/Findarato/dotFiles/master/.ansible.cfg" -O "$HOME/.ansible.cfg"
+wget "https://raw.githubusercontent.com/Findarato/dotFiles/master/.bashrc" -O "${HOME}/.bashrc"
+wget "https://raw.githubusercontent.com/Findarato/dotFiles/master/.ansible.cfg" -O "${HOME}/.ansible.cfg"
 
-#source "$HOME/.bashrc"
-#export ANSIBLE_VAULT_PASSWORD_FILE=~/.vault_pass.txt
+
+git clone git@github.com:Findarato/pass.git "${HOME}/.password-store"
 
 echo "Starting Deployment"
-cd "$SETUPDIR"
-git clone "https://github.com/Findarato/Ansible-Workstation.git" "$SETUPDIR/Ansible-Workstaton"
+cd "${SETUPDIR}"
+git clone "https://github.com/Findarato/Ansible-Workstation.git" "${SETUPDIR}/Ansible-Workstaton"
 
 echo "Chaning to Ansible directory"
-cd "$SETUPDIR/Ansible-Workstaton"
+cd "${SETUPDIR}/Ansible-Workstaton"
 
 echo "Setting up Ansible Roles"
 ansible-galaxy install -r roles/requirements.yml
 
 echo "Running Ansible"
-ansible-playbook -i localHosts deploy.yml --extra-vars "local_user=$USER" --vault-id "${HOME}/bin/ansiblePass.sh"
+ansible-playbook -i localHosts deploy.yml --extra-vars "local_user=${USER}" --vault-id "${HOME}/bin/ansiblePass.sh"
 
 #Clean up the Setup
 echo "Cleaning up Setup directory"
-#rm -rf $HOME/setup
+#rm -rf ${HOME}/setup
