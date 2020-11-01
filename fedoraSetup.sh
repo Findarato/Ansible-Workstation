65d10eaade6cb96ede264c5fd9cb44315642bcd8
Joseph Harry <findarato@gmail.com>
2020-11-01 13:27:05 -0600

    Updated Signed-off-by: Joseph Harry <findarato@gmail.com>

--- fedoraSetup.sh
+++ fedoraSetup.sh
@@ -31,7 +31,7 @@ echo "Setting up Ansible Roles"
 ansible-galaxy install -r "${SETUPDIR}/Ansible-Workstation/roles/requirements.yml"
 
 echo "Running Ansible"
-ansible-playbook -i localHosts deploy.yml --extra-vars "local_user=${USER}" --ask-become
+ansible-playbook -i localHosts deploy.yml --extra-vars '{"local_user=${USER}"}' --ask-become
 
 #Clean up the Setup
 echo "Cleaning up Setup directory"
