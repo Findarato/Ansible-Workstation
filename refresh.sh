837b720d3a7e01734d03da6e7713a3b6f44b1626
Joseph Harry <findarato@gmail.com>
2021-10-26 07:54:03 -0500

    Update Refresh Signed-off-by: Joseph Harry <findarato@gmail.com>

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
-echo "Running Ansible"
-ansible-playbook -i localHosts setup_computer.yml --extra-var local_user=${USER}
+
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+# echo "${red}Ansible ${reset}"
+
+ANSIBLE<<InputComesFromHERE
+     _              _ _     _
+    / \   _ __  ___(_) |__ | | ___
+   / _ \ | '_ \/ __| | '_ \| |/ _ \
+  / ___ \| | | \__ \ | |_) | |  __/
+ /_/   \_\_| |_|___/_|_.__/|_|\___|
+InputComesFromHERE
+
+echo ${ANSIBLE}
+# ansible-playbook -i localHosts setup_computer.yml --extra-var local_user=${USER}
