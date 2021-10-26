b63e5903b8e30d81de4c02c7b68daa97a1429e3f
Joseph Harry <findarato@gmail.com>
2021-10-26 09:31:17 -0500

    Updating Signed-off-by: Joseph Harry <findarato@gmail.com>

--- refresh.sh
+++ refresh.sh
@@ -5,15 +5,15 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-# echo "${red}Ansible ${reset}"
+echo "${red}Ansible ${reset}"
 
-ANSIBLE<<InputComesFromHERE
-     _              _ _     _
-    / \   _ __  ___(_) |__ | | ___
-   / _ \ | '_ \/ __| | '_ \| |/ _ \
-  / ___ \| | | \__ \ | |_) | |  __/
- /_/   \_\_| |_|___/_|_.__/|_|\___|
-InputComesFromHERE
+# ANSIBLE<<InputComesFromHERE
+#      _              _ _     _
+#     / \   _ __  ___(_) |__ | | ___
+#    / _ \ | '_ \/ __| | '_ \| |/ _ \
+#   / ___ \| | | \__ \ | |_) | |  __/
+#  /_/   \_\_| |_|___/_|_.__/|_|\___|
+# InputComesFromHERE
 
-echo ${ANSIBLE}
-# ansible-playbook -i localHosts setup_computer.yml --extra-var local_user=${USER}
+# echo ${ANSIBLE}
+ansible-playbook -i localHosts setup_computer.yml --extra-var local_user=${USER}
