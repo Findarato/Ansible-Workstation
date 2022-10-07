53d823e54a4f11c05850f0bc75223a84585d6987
Joseph Harry <findarato@gmail.com>
2022-10-07 10:49:21 -0500

    :sparkle: Updated yubikey stuff Signed-off-by: Joseph Harry <findarato@gmail.com>

--- roles/base/files/settings.sh
+++ roles/base/files/settings.sh
@@ -15,29 +15,27 @@
 
 
 
-
-
 gsettings set org.gnome.settings-daemon.plugins.media-keys terminal '<Super>t'
 gsettings set org.gnome.settings-daemon.plugins.media-keys home '<Super>e'
 
+# Bluetooth thing
 
+gsettings set org.gnome.shell had-bluetooth-devices-setup true
 
-
-
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f use-theme-colors false
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f foreground-color '#B8A898'
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f background-color '#2A211C'
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f dim-color-set false
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f bold-color-set false
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f badge-color-set false
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f highlight-colors-set false
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f cursor-colors-set false
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f palette ['#000000', '#CC0000', '#1A921C', '#F0E53A', '#0066FF', '#C5656B', '#06989A', '#D3D7CF', '#555753', '#EF2929', '#9AFF87', '#FFFB5C', '#43A8ED', '#FF818A', '#34E2E2', '#EEEEEC']
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f visible-name 'Joe Terminal'
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f font 'Hack 12'
-gsettings set com.gexperts.Tilix.window-state 128
-gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f background-transparency-percent 4
-gsettings set com.gexperts.Tilix.ProfilesList default '6194e50a-0bab-498b-aeee-b88a298a6f0f'
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f use-theme-colors false
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f foreground-color '#B8A898'
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f background-color '#2A211C'
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f dim-color-set false
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f bold-color-set false
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f badge-color-set false
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f highlight-colors-set false
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f cursor-colors-set false
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f palette ['#000000', '#CC0000', '#1A921C', '#F0E53A', '#0066FF', '#C5656B', '#06989A', '#D3D7CF', '#555753', '#EF2929', '#9AFF87', '#FFFB5C', '#43A8ED', '#FF818A', '#34E2E2', '#EEEEEC']
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f visible-name 'Joe Terminal'
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f font 'Hack 12'
+# gsettings set com.gexperts.Tilix.window-state 128
+# gsettings set com.gexperts.Tilix.profiles.6194e50a-0bab-498b-aeee-b88a298a6f0f background-transparency-percent 4
+# gsettings set com.gexperts.Tilix.ProfilesList default '6194e50a-0bab-498b-aeee-b88a298a6f0f'
 
 
 # Confermed to work
@@ -60,11 +58,11 @@ gsettings set org.gnome.desktop.peripherals.touchpad tap-and-drag true
 gsettings set org.gnome.desktop.peripherals.touchpad edge-scrolling-enabled false
 org.gnome.desktop.peripherals.touchpad disable-while-typing true
 
-#Custom Shortcut for Tilix
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings ['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Launch Tilix'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Primary><Alt>t'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'tilix'
+# #Custom Shortcut for Tilix
+# gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings ['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Launch Tilix'
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Primary><Alt>t'
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'tilix'
 
 
 gsettings set org.gnome.shell. enabled-extensions ['background-logo@fedorahosted.org', 'nvidiatemp@baco', 'TopIcons@phocean.net', 'user-theme@gnome-shell-extensions.gcampax.github.com','calc@patapon.info']
